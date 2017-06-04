edf9bd4608af035061fa3c5a174ea7cfccf44560
Jeffrey Zhang <jzhang11235@gmail.com>
2017-06-05 02:17:04 +1000

    Rearrange GUI elements and begin on GameController class

--- src/main/java/com/github/javabada/baccarat/game/Game.java
+++ src/main/java/com/github/javabada/baccarat/game/Game.java
@@ -2,52 +2,8 @@ package com.github.javabada.baccarat.game;
 
 public class Game {
 
-    private static final int WAGER_LOW_MIN  = 5;
-    private static final int WAGER_LOW_MAX  = 500;
-    private static final int WAGER_MED_MIN  = 100;
-    private static final int WAGER_MED_MAX  = 10000;
-    private static final int WAGER_HIGH_MIN = 1000;
-    private static final int WAGER_HIGH_MAX = 150000;
-
-    private int minWager;
-    private int maxWager;
-    private Player player;
-
-    public Game() {
-    }
-
-    public void setLowWager() {
-        minWager = WAGER_LOW_MIN;
-        maxWager = WAGER_LOW_MAX;
-    }
-
-    public void setMedWager() {
-        minWager = WAGER_MED_MIN;
-        maxWager = WAGER_MED_MAX;
-    }
-
-    public void setHighWager() {
-        minWager = WAGER_HIGH_MIN;
-        maxWager = WAGER_HIGH_MAX;
-    }
-
-    public int getMinWager() {
-        return minWager;
-    }
-
-    public int getMaxWager() {
-        return maxWager;
-    }
-
-
-
-
-    /*
-     * TODO
-     * burn()
-     * newShoe()
-     *
-     */
-
+    // TODO
+    // burn()
+    // change shoe / cut card
 
 }
--- src/main/java/com/github/javabada/baccarat/game/GameController.java
+++ src/main/java/com/github/javabada/baccarat/game/GameController.java
@@ -1,4 +1,25 @@
 package com.github.javabada.baccarat.game;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class GameController {
+
+    private Player player;
+
+    private StringProperty balanceNumber;
+
+    @FXML
+    private Label balanceNumberLabel;
+
+    @FXML
+    private void initialize() {
+        player = new Player("10000");
+
+        balanceNumber = new SimpleStringProperty("$" + player.getBalanceString());
+        balanceNumberLabel.textProperty().bind(balanceNumber);
+    }
+
 }
