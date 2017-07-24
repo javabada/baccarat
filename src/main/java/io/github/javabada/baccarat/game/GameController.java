a925e20bda9eaf67005fbe70ea67bda6079e1a13
Jeffrey Zhang <jzhang11235@gmail.com>
2017-07-24 18:17:04 +1000

    Add place and clear wager functionality

--- src/main/java/io/github/javabada/baccarat/game/GameController.java
+++ src/main/java/io/github/javabada/baccarat/game/GameController.java
@@ -1,22 +1,26 @@
 package io.github.javabada.baccarat.game;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import java.text.NumberFormat;
 import java.util.Locale;
 
 public class GameController {
 
-  private Player player = new Player("10000");
+  private Player player;
 
   @FXML private Label balanceLabel;
   @FXML private Label messageLabel;
-  @FXML private Button actionButton;
   @FXML private Button tieButton;
-  @FXML private Button playerButton;
   @FXML private Button bankerButton;
+  @FXML private Button playerButton;
+  @FXML private Button playButton;
+  @FXML private Button clearButton;
+  @FXML private ToggleGroup chipsToggleGroup;
   @FXML private ImageView playerCard1;
   @FXML private ImageView playerCard2;
   @FXML private ImageView playerCard3;
@@ -26,11 +30,62 @@ public class GameController {
 
   @FXML
   private void initialize() {
+    player = new Player("10000");
+
     balanceLabel.setText(formatCurrency(player.getBalance()));
-    actionButton.setText("Play");
-    tieButton.setText("Tie");
-    playerButton.setText("Player");
-    bankerButton.setText("Banker");
+    tieButton.setText(tieButton.getUserData().toString());
+    bankerButton.setText(bankerButton.getUserData().toString());
+    playerButton.setText(playerButton.getUserData().toString());
+    playButton.setText("Play");
+    clearButton.setDisable(true);
+  }
+
+  @FXML
+  private void placeWagerAction(ActionEvent event) {
+    if (chipsToggleGroup.getSelectedToggle() == null) {
+      messageLabel.setText("Please select a chip");
+      return;
+    }
+
+    String chipValue = chipsToggleGroup.getSelectedToggle().getUserData().toString();
+
+    Button wagerButton;
+    Outcome wagerOutcome;
+
+    if (event.getSource() == tieButton) {
+      wagerButton = tieButton;
+      wagerOutcome = Outcome.TIE;
+    } else if (event.getSource() == bankerButton) {
+      wagerButton = bankerButton;
+      wagerOutcome = Outcome.BANKER;
+    } else {
+      wagerButton = playerButton;
+      wagerOutcome = Outcome.PLAYER;
+    }
+
+    boolean wagerPlaced = player.placeWager(wagerOutcome, chipValue);
+
+    if (wagerPlaced) {
+      balanceLabel.setText(formatCurrency(player.getBalance()));
+      messageLabel.setText("");
+      wagerButton.setText(wagerButton.getUserData().toString() + "\n" +
+          formatCurrency(player.checkWager(wagerOutcome)));
+      clearButton.setDisable(false);
+    } else {
+      messageLabel.setText("Insufficient balance");
+    }
+  }
+
+  @FXML
+  private void clearAction() {
+    player.clearWagers();
+
+    balanceLabel.setText(formatCurrency(player.getBalance()));
+    messageLabel.setText("");
+    tieButton.setText(tieButton.getUserData().toString());
+    bankerButton.setText(bankerButton.getUserData().toString());
+    playerButton.setText(playerButton.getUserData().toString());
+    clearButton.setDisable(true);
   }
 
   private String formatCurrency(Object o) {
@@ -40,77 +95,6 @@ public class GameController {
 }
 
   /*
-  private Player player;
-  private Coup coup;
-
-  @FXML private Label messageLabel;
-  @FXML private Label balanceNumberLabel;
-  @FXML private Button actionButton;
-  @FXML private Button clearButton;
-  @FXML private Button tieButton;
-  @FXML private Button bankerButton;
-  @FXML private Button playerButton;
-  @FXML private ToggleGroup chipToggleGroup;
-
-  @FXML private ImageView playerCardView1;
-  @FXML private ImageView playerCardView2;
-  @FXML private ImageView playerCardView3;
-  @FXML private ImageView bankerCardView1;
-  @FXML private ImageView bankerCardView2;
-  @FXML private ImageView bankerCardView3;
-
-  @FXML
-  private void initialize() {
-    player = new Player();
-    coup = new Coup(game.getShoe());
-
-    balanceNumberLabel.setText(formatCurrency(player.getBalance()));
-    actionButton.setText("Deal");
-    clearButton.setDisable(true);
-    tieButton.setText("Tie");
-    bankerButton.setText("Banker");
-    playerButton.setText("Player");
-  }
-
-  @FXML
-  private void handlePlaceWagerButtonAction(ActionEvent event) {
-    if (chipToggleGroup.getSelectedToggle() == null) {
-      messageLabel.setText("No chips selected");
-      return;
-    }
-
-    messageLabel.setText("");
-    clearButton.setDisable(false);
-
-    String wagerAmount = chipToggleGroup.getSelectedToggle().getUserData().toString();
-    boolean wagerPlaced;
-
-    if (event.getSource() == tieButton) {
-      wagerPlaced = player.placeWager(Outcome.TIE, wagerAmount);
-      if (wagerPlaced) {
-        tieButton.setText("Tie\n" + formatCurrency(player.checkWager(Outcome.TIE)));
-      }
-    }
-    else if (event.getSource() == bankerButton) {
-      wagerPlaced = player.placeWager(Outcome.BANKER, wagerAmount);
-      if (wagerPlaced) {
-        bankerButton.setText("Banker\n" + formatCurrency(player.checkWager(Outcome.BANKER)));
-      }
-    }
-    else {
-      wagerPlaced = player.placeWager(Outcome.PLAYER, wagerAmount);
-      if (wagerPlaced) {
-        playerButton.setText("Player\n" + formatCurrency(player.checkWager(Outcome.PLAYER)));
-      }
-    }
-
-    if (!wagerPlaced) {
-      messageLabel.setText("Not enough money");
-    }
-
-    balanceNumberLabel.setText(formatCurrency(player.getBalance()));
-  }
-
   @FXML
   private void handleActionButtonAction(ActionEvent event) {
     if (coup.isFinished()) {
