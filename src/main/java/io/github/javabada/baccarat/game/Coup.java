d656c8967f8888fe9b3a9fe385f71f08bb6b22a4
Jeffrey Zhang <jzhang11235@gmail.com>
2018-01-27 11:04:37 +1100

    Update Coup and CoupTest

--- src/main/java/io/github/javabada/baccarat/game/Coup.java
+++ src/main/java/io/github/javabada/baccarat/game/Coup.java
@@ -1,7 +1,5 @@
 package io.github.javabada.baccarat.game;
 
-// A coup is a round of play in baccarat.
-
 public class Coup {
 
   private final Shoe shoe;
@@ -11,10 +9,10 @@ public class Coup {
 
   private Card playerCard1;
   private Card playerCard2;
-  //private Card playerCard3;
   private Card bankerCard1;
   private Card bankerCard2;
-  //private Card bankerCard3;
+
+  private boolean coupFinished = false;
 
   public Coup(Shoe shoe) {
     this.shoe = shoe;
@@ -28,9 +26,12 @@ public class Coup {
 
     playerScore = (playerCard1.getValue() + playerCard2.getValue()) % 10;
     bankerScore = (bankerCard1.getValue() + bankerCard2.getValue()) % 10;
+
+    coupFinished = true;
   }
 
   public Outcome getOutcome() {
+    checkState();
     if (playerScore > bankerScore) {
       return Outcome.PLAYER;
     } else if (bankerScore > playerScore) {
@@ -40,4 +41,110 @@ public class Coup {
     }
   }
 
+  public int getPlayerScore() {
+    checkState();
+    return playerScore;
+  }
+
+  public int getBankerScore() {
+    checkState();
+    return bankerScore;
+  }
+
+  public Card getPlayerCard1() {
+    checkState();
+    return playerCard1;
+  }
+
+  public Card getPlayerCard2() {
+    checkState();
+    return playerCard2;
+  }
+
+  public Card getBankerCard1() {
+    checkState();
+    return bankerCard1;
+  }
+
+  public Card getBankerCard2() {
+    checkState();
+    return bankerCard2;
+  }
+
+  private void checkState() {
+    if (!coupFinished) {
+      throw new IllegalStateException("Coup not played");
+    }
+  }
+
 }
+
+// package io.github.javabada.baccarat.game;
+//
+// // A coup is a round of play in baccarat.
+//
+// public class Coup {
+//
+//   private final Shoe shoe;
+//
+//   private int playerScore = 0;
+//   private int bankerScore = 0;
+//
+//   private Card playerCard1;
+//   private Card playerCard2;
+//   private Card playerCard3;
+//   private Card bankerCard1;
+//   private Card bankerCard2;
+//   private Card bankerCard3;
+//
+//   public Coup(Shoe shoe) {
+//     this.shoe = shoe;
+//   }
+//
+//   public void play() {
+//     playerCard1 = shoe.draw();
+//     bankerCard1 = shoe.draw();
+//     playerCard2 = shoe.draw();
+//     bankerCard2 = shoe.draw();
+//
+//     playerScore = (playerCard1.getValue() + playerCard2.getValue()) % 10;
+//     bankerScore = (bankerCard1.getValue() + bankerCard2.getValue()) % 10;
+//
+//     if (playerScore > 7 || bankerScore > 7) {
+//       return;
+//     }
+//
+//     // TODO: Use Optional
+//
+//     if (playerScore < 6) {
+//       playerCard3 = shoe.draw();
+//       playerScore = (playerScore + playerCard3.getValue()) % 10;
+//     }
+//
+//     if (playerCard3 == null) {
+//       if (bankerScore < 6) {
+//         bankerCard3 = shoe.draw();
+//         bankerScore = (bankerScore + bankerCard3.getValue()) % 10;
+//       }
+//     }
+//   }
+//
+//   public Outcome getOutcome() {
+//     if (playerScore > bankerScore) {
+//       return Outcome.PLAYER;
+//     } else if (bankerScore > playerScore) {
+//       return Outcome.BANKER;
+//     } else {
+//       return Outcome.TIE;
+//     }
+//   }
+//
+//   public int getPlayerScore() {
+//     return playerScore;
+//   }
+//
+//   public int getBankerScore() {
+//     return bankerScore;
+//   }
+//
+// }
--- src/test/java/io/github/javabada/baccarat/game/CoupTest.java
+++ src/test/java/io/github/javabada/baccarat/game/CoupTest.java
@@ -1,18 +1,14 @@
 package io.github.javabada.baccarat.game;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-// Following behaviours are tested for each coup:
-// - Outcome
-// - Amount of cards dealt
 
 class CoupTest {
 
@@ -26,57 +22,212 @@ class CoupTest {
   }
 
   @Test
-  @DisplayName("Natural, player win")
-  void coupTest1() {
+  void playerWinWithNatural8() {
     when(shoe.draw()).thenReturn(
       new Card(Rank.TEN, Suit.CLUBS),
       new Card(Rank.TWO, Suit.CLUBS),
-      new Card(Rank.NINE, Suit.CLUBS),
-      new Card(Rank.JACK, Suit.CLUBS),
-      new Card(Rank.ACE, Suit.CLUBS),
-      new Card(Rank.ACE, Suit.CLUBS)
+      new Card(Rank.EIGHT, Suit.CLUBS),
+      new Card(Rank.QUEEN, Suit.CLUBS)
     );
-
     coup.play();
-
     assertEquals(Outcome.PLAYER, coup.getOutcome());
-    verify(shoe, times(4)).draw();
   }
 
   @Test
-  @DisplayName("Natural, banker win")
-  void coupTest2() {
+  void bankerWinWithNatural9() {
     when(shoe.draw()).thenReturn(
       new Card(Rank.TEN, Suit.CLUBS),
       new Card(Rank.TWO, Suit.CLUBS),
-      new Card(Rank.SIX, Suit.CLUBS),
-      new Card(Rank.SIX, Suit.CLUBS),
-      new Card(Rank.ACE, Suit.CLUBS),
-      new Card(Rank.ACE, Suit.CLUBS)
+      new Card(Rank.EIGHT, Suit.CLUBS),
+      new Card(Rank.SEVEN, Suit.CLUBS)
     );
-
     coup.play();
-
     assertEquals(Outcome.BANKER, coup.getOutcome());
-    verify(shoe, times(4)).draw();
   }
 
   @Test
-  @DisplayName("Natural, tie")
-  void coupTest3() {
+  void tieWithNatural8() {
     when(shoe.draw()).thenReturn(
-      new Card(Rank.TEN, Suit.CLUBS),
       new Card(Rank.TWO, Suit.CLUBS),
-      new Card(Rank.NINE, Suit.CLUBS),
-      new Card(Rank.SEVEN, Suit.CLUBS),
-      new Card(Rank.ACE, Suit.CLUBS),
-      new Card(Rank.ACE, Suit.CLUBS)
+      new Card(Rank.TWO, Suit.CLUBS),
+      new Card(Rank.SIX, Suit.CLUBS),
+      new Card(Rank.SIX, Suit.CLUBS)
     );
-
     coup.play();
-
     assertEquals(Outcome.TIE, coup.getOutcome());
-    verify(shoe, times(4)).draw();
+  }
+
+  @Test
+  void scoresSumCorrectly() {
+    when(shoe.draw()).thenReturn(
+      new Card(Rank.NINE, Suit.CLUBS),
+      new Card(Rank.NINE, Suit.CLUBS),
+      new Card(Rank.NINE, Suit.CLUBS),
+      new Card(Rank.FIVE, Suit.CLUBS)
+    );
+    coup.play();
+    assertEquals(8, coup.getPlayerScore());
+    assertEquals(4, coup.getBankerScore());
+  }
+
+  @Test
+  void getOutcomeBeforeCoupPlayThrowsException() {
+    Throwable exception = assertThrows(IllegalStateException.class, () -> {
+      coup.getOutcome();
+    });
+    assertEquals("Coup not played", exception.getMessage());
+  }
+
+  @Test
+  void getScoreBeforeCoupPlayThrowsException() {
+    Throwable exception = assertThrows(IllegalStateException.class, () -> {
+      coup.getPlayerScore();
+    });
+    assertEquals("Coup not played", exception.getMessage());
+  }
+
+  @Test
+  void getCardBeforeCoupPlayThrowsException() {
+    Throwable exception = assertThrows(IllegalStateException.class, () -> {
+      coup.getPlayerCard1();
+    });
+    assertEquals("Coup not played", exception.getMessage());
   }
 
 }
+
+// package io.github.javabada.baccarat.game;
+//
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+// import static org.mockito.Mockito.mock;
+// import static org.mockito.Mockito.times;
+// import static org.mockito.Mockito.verify;
+// import static org.mockito.Mockito.when;
+//
+// import org.junit.jupiter.api.BeforeEach;
+// import org.junit.jupiter.api.DisplayName;
+// import org.junit.jupiter.api.Test;
+//
+// class CoupTest {
+//
+//   Shoe shoe;
+//   Coup coup;
+//
+//   @BeforeEach
+//   void init() {
+//     shoe = mock(Shoe.class);
+//     coup = new Coup(shoe);
+//   }
+//
+//   // Following tests check behaviour for various coup scenarios
+//
+//   @Test
+//   @DisplayName("Natural, player win")
+//   void coupTest1() {
+//     when(shoe.draw()).thenReturn(
+//       new Card(Rank.TEN, Suit.CLUBS),
+//       new Card(Rank.TWO, Suit.CLUBS),
+//       new Card(Rank.NINE, Suit.CLUBS),
+//       new Card(Rank.JACK, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS)
+//     );
+//
+//     coup.play();
+//
+//     assertEquals(Outcome.PLAYER, coup.getOutcome());
+//     verify(shoe, times(4)).draw();
+//   }
+//
+//   @Test
+//   @DisplayName("Natural, banker win")
+//   void coupTest2() {
+//     when(shoe.draw()).thenReturn(
+//       new Card(Rank.TEN, Suit.CLUBS),
+//       new Card(Rank.TWO, Suit.CLUBS),
+//       new Card(Rank.SIX, Suit.CLUBS),
+//       new Card(Rank.SIX, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS)
+//     );
+//
+//     coup.play();
+//
+//     assertEquals(Outcome.BANKER, coup.getOutcome());
+//     verify(shoe, times(4)).draw();
+//   }
+//
+//   @Test
+//   @DisplayName("Natural, tie")
+//   void coupTest3() {
+//     when(shoe.draw()).thenReturn(
+//       new Card(Rank.TEN, Suit.CLUBS),
+//       new Card(Rank.TWO, Suit.CLUBS),
+//       new Card(Rank.NINE, Suit.CLUBS),
+//       new Card(Rank.SEVEN, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS)
+//     );
+//
+//     coup.play();
+//
+//     assertEquals(Outcome.TIE, coup.getOutcome());
+//     verify(shoe, times(4)).draw();
+//   }
+//
+//   @Test
+//   @DisplayName("Both stand, player win")
+//   void coupTest4() {
+//     when(shoe.draw()).thenReturn(
+//       new Card(Rank.TWO, Suit.CLUBS),
+//       new Card(Rank.TWO, Suit.CLUBS),
+//       new Card(Rank.FIVE, Suit.CLUBS),
+//       new Card(Rank.FOUR, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS)
+//     );
+//
+//     coup.play();
+//
+//     assertEquals(Outcome.PLAYER, coup.getOutcome());
+//     verify(shoe, times(4)).draw();
+//   }
+//
+//   @Test
+//   @DisplayName("Player stands and banker draws, player win")
+//   void coupTest5() {
+//     when(shoe.draw()).thenReturn(
+//       new Card(Rank.TWO, Suit.CLUBS),
+//       new Card(Rank.TWO, Suit.CLUBS),
+//       new Card(Rank.FIVE, Suit.CLUBS),
+//       new Card(Rank.TWO, Suit.CLUBS),
+//       new Card(Rank.NINE, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS)
+//     );
+//
+//     coup.play();
+//
+//     assertEquals(Outcome.PLAYER, coup.getOutcome());
+//     verify(shoe, times(5)).draw();
+//   }
+//
+//   // Following tests check other functionalities
+//
+//   @Test
+//   void cardValuesSumCorrectly() {
+//     when(shoe.draw()).thenReturn(
+//       new Card(Rank.NINE, Suit.CLUBS),
+//       new Card(Rank.NINE, Suit.CLUBS),
+//       new Card(Rank.NINE, Suit.CLUBS),
+//       new Card(Rank.FIVE, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS),
+//       new Card(Rank.ACE, Suit.CLUBS)
+//     );
+//
+//     coup.play();
+//
+//     assertEquals(8, coup.getPlayerScore());
+//     assertEquals(4, coup.getBankerScore());
+//   }
+//
+// }
